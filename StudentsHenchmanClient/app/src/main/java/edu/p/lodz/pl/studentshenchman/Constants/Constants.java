74016ffdf3608ab6ac039027a06478ac44f52ce1
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-05 23:21:10 +0200

    Dodanie Constantow
    Stały bazowy URL

---
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/Constants/Constants.java
@@ -0,0 +1,13 @@
+package edu.p.lodz.pl.studentshenchman.Constants;
+
+/**
+ * Created by Michał on 2016-10-05.
+ */
+public abstract class Constants {
+
+    public static final String PROTOCOL = "http";
+    public static final String HOST = "localhost";
+    public static final int PORT = 8080;
+
+    public static final String BASE_SERVER_URL = PROTOCOL + "://" + HOST + ":" + PORT + "/";
+}
