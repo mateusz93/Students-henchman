808bf96384455f089a39a636b5ea3c69449c43c4
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-09 00:17:07 +0200

    Głowne klasy widokow: Activity - StudentShenchmanMainActivity Fragment - StudentShenchmanMainActivity

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/MainActivity.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/MainActivity.java
@@ -2,15 +2,15 @@ package edu.p.lodz.pl.studentshenchman;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import edu.p.lodz.pl.studentshenchman.abstract_ui.StudentShenchmanMainActivity;
 import edu.p.lodz.pl.studentshenchman.workers.DownloadWeatherSimpleWorker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends StudentShenchmanMainActivity {
 
     /**
      * Metoda tworzy aktywnosc.
@@ -31,54 +31,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     * Metoda niszczy aktywnosc.
-     */
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    /**
-     * Metoda dzialajaca w trakcie aplikacji.
-     */
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    /**
-     * Metoda dzialajaca w trakcie aplikacji.
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    /**
-     * Metoda przywracajace procesi i czynnosci, ktore zostaly zatrzymane przez wywolanie onStop().
-     */
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    /**
-     * Metoda dzialajaca w trakcie aplikacji.
-     */
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-    }
-
-    /**
-     * Metoda zatrzymuje kazda aktywnosc, dotyczy to interfejsu, animacji, watkow i serwisow.
-     */
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -87,18 +39,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Intent anotherActivity;
-
         switch (item.getItemId()) {
             case R.id.action_settings:
-                anotherActivity = new Intent(MainActivity.this, SettingsActivity.class);
-                finish();
-
-                startActivity(anotherActivity);
+                goToSettings();
                 return true;
 
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    private void goToSettings() {
+        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
 }
--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/SettingsActivity.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/SettingsActivity.java
@@ -1,8 +1,8 @@
 package edu.p.lodz.pl.studentshenchman;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -15,59 +15,15 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
     }
 
-    /**
-     * Metoda niszczy aktywnosc.
-     */
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public void onBackPressed() {
+        goToDashBoard();
     }
 
-    /**
-     * Metoda dzialajaca w trakcie aplikacji.
-     */
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    /**
-     * Metoda dzialajaca w trakcie aplikacji.
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    /**
-     * Metoda przywracajace procesi i czynnosci, ktore zostaly zatrzymane przez wywolanie onStop().
-     */
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    /**
-     * Metoda dzialajaca w trakcie aplikacji.
-     */
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-    }
-
-    /**
-     * Metoda zatrzymuje kazda aktywnosc, dotyczy to interfejsu, animacji, watkow i serwisow.
-     */
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    public void onBackPressed()
-    {
+    private void goToDashBoard() {
         Intent previousActivity = new Intent(SettingsActivity.this, MainActivity.class);
         finish();
         startActivity(previousActivity);
     }
+
 }
