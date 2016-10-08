57170d0ca9a247f895289e13e6e0521a79d96a8d
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-08 18:28:19 +0200

    Dodatkowe stale

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/Constants/Constants.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/Constants/Constants.java
@@ -5,9 +5,17 @@ package edu.p.lodz.pl.studentshenchman.Constants;
  */
 public abstract class Constants {
 
+    public static final String DATE_FORMT = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'";
+
+
     public static final String PROTOCOL = "http";
     public static final String HOST = "localhost";
     public static final int PORT = 8080;
 
-    public static final String BASE_SERVER_URL = PROTOCOL + "://" + HOST + ":" + PORT + "/";
+    public static final int READ_TIMEOUT = 30;
+    public static final int CONNECTION_TIMEOUT = 30;
+    public static final int WRITE_TIMEOUT = 30;
+
+    public static final String BASE_SERVER_URL = PROTOCOL + "://" + HOST + ":" + PORT + "/mobile";
+
 }
