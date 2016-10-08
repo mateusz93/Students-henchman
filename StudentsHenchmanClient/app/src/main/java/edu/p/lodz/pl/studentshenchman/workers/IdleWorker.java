b888507f9db5876116ad52e300b5247561c22cd7
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-09 00:04:18 +0200

    IdleWorker - worker, który nic nie robi ale sie odpala i wyłącza
    Zostaje zwrocony przez WorkerFactory jezeli factorka nie znajdzie odpowiedniego typu workera

---
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/workers/IdleWorker.java
@@ -0,0 +1,43 @@
+package edu.p.lodz.pl.studentshenchman.workers;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Michał on 2016-10-05.
+ */
+public class IdleWorker extends AbstractWorker {
+    public static final String TAG = IdleWorker.class.getName();
+
+    private final Context mContext;
+
+    public IdleWorker(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void sendResponse(Intent responseIntent) {
+
+    }
+
+    @Override
+    public void run() {
+        Log.i(TAG, "Idle service - nothing to do");
+    }
+
+    @Override
+    public void onCompleted() {
+        Log.i(TAG, "Idle worker - onCompleted");
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        onError(mContext, e.getMessage());
+    }
+
+    @Override
+    public void onNext(Object o) {
+        Log.i(TAG, "Idle worker - onNext - nothing to do just finish it");
+    }
+}
