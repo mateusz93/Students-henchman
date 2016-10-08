f7310cf566e6293781df0929fc3186c3b3f5a100
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-09 01:11:18 +0200

    Dodanie activity do manifestu, dodanie toolbara do glownego activity

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/MainActivity.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/MainActivity.java
@@ -2,21 +2,29 @@ package edu.p.lodz.pl.studentshenchman;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import edu.p.lodz.pl.studentshenchman.abstract_ui.StudentShenchmanMainActivity;
+import edu.p.lodz.pl.studentshenchman.utils.Utils;
 import edu.p.lodz.pl.studentshenchman.workers.DownloadWeatherSimpleWorker;
 
 public class MainActivity extends StudentShenchmanMainActivity {
     private static final String TAG = MainActivity.class.getName();
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        Utils.prepareToolbar(toolbar, R.string.dashboard, android.R.drawable.btn_plus);
+        setSupportActionBar(toolbar);
 
         Button button = (Button) findViewById(R.id.button);
 
