9282c98febcc76d7bdd357334775895ce5ccd325
milw0rm <andrzej@miedzinski.net>
2016-10-09 22:00:12 +0200

    Pojawiajacy sie / znikajacy EditText dla tokenow

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/ReadDataActivity.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/ReadDataActivity.java
@@ -3,6 +3,13 @@ package edu.p.lodz.pl.studentshenchman;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TableRow;
 
 import edu.p.lodz.pl.studentshenchman.abstract_ui.StudentShenchmanMainActivity;
 
@@ -13,6 +20,21 @@ public class ReadDataActivity extends StudentShenchmanMainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_data);
+
+        final TableRow tokenValueTableRow = (TableRow) findViewById(R.id.tokenValueTableRow);
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.tokenRadioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.tokenAvailable) {
+                    tokenValueTableRow.setVisibility(View.VISIBLE);
+                } else if (checkedId == R.id.tokenNotAvailable) {
+                    tokenValueTableRow.setVisibility(View.GONE);
+                }
+            }
+        });
+
     }
 
     @Override
