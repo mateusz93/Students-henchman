b8c56925e6cf7017aa415380bd7c890cc2af66ce
Mateusz Wieczorek <wskmateusz@gmail.com>
2016-10-09 22:58:21 +0200

    Dodanie Basic Authentication do serwisów

--- StudentsHenchmanServiceBus/src/main/java/configuration/Application.java
+++ StudentsHenchmanServiceBus/src/main/java/configuration/Application.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @Author Mateusz Wieczorek on 9/30/16.
@@ -20,13 +23,28 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan({"controller", "service"})
 @EntityScan("model")
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
     }
+}
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated().and().
+                httpBasic().and().
+                csrf().disable();
+    }
+
 }
