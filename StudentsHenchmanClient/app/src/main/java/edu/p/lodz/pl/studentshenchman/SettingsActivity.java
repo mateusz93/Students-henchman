2fdb2f08adc0e693ee86e4857c5b65b3c872227e
milw0rm <andrzej@miedzinski.net>
2016-10-10 19:32:25 +0200

    Kaskadowe ustawienia

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/SettingsActivity.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/SettingsActivity.java
@@ -21,17 +21,62 @@ public class SettingsActivity extends StudentShenchmanMainActivity {
         setContentView(R.layout.activity_settings);
 
         Spinner facultySelectBox = (Spinner) findViewById(R.id.facultySelectBox);
-        Spinner typeSelectBox = (Spinner) findViewById(R.id.typeSelectBox);
-        Spinner subjectSelectBox = (Spinner) findViewById(R.id.subjectSelectBox);
+        final Spinner typeSelectBox = (Spinner) findViewById(R.id.typeSelectBox);
+        final Spinner subjectSelectBox = (Spinner) findViewById(R.id.subjectSelectBox);
 
         final TableRow typeTableRow = (TableRow) findViewById(R.id.typeTableRow);
         final TableRow subjectTableRow = (TableRow) findViewById(R.id.subjectTableRow);
         final TableRow majorTableRow = (TableRow) findViewById(R.id.majorTableRow);
         final TableRow readButtonTableRow = (TableRow) findViewById(R.id.readButtonTableRow);
 
+        typeTableRow.setVisibility(View.GONE);
+        subjectTableRow.setVisibility(View.GONE);
+        majorTableRow.setVisibility(View.GONE);
+        readButtonTableRow.setVisibility(View.GONE);
+
+        //String provider = spinner1.getSelectedItem().toString();
+
         facultySelectBox.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                typeTableRow.setVisibility(View.VISIBLE);
+                typeSelectBox.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                        subjectTableRow.setVisibility(View.VISIBLE);
+                        subjectSelectBox.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                                majorTableRow.setVisibility(View.VISIBLE);
+
+                                // WYBOR SPECJALIZACJI
+
+                                readButtonTableRow.setVisibility(View.GONE);
+
+                                // JAK CHOCIAZ JEDNO WYBIORE
+                                // readButtonTableRow.setVisibility(View.VISIBLE);
+
+                            }
+
+                            @Override
+                            public void onNothingSelected(AdapterView<?> parentView) {
+                                majorTableRow.setVisibility(View.GONE);
+                                readButtonTableRow.setVisibility(View.GONE);
+                            }
+
+                        });
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parentView) {
+                        subjectTableRow.setVisibility(View.GONE);
+                        majorTableRow.setVisibility(View.GONE);
+                        readButtonTableRow.setVisibility(View.GONE);
+                    }
+
+                });
 
             }
 
