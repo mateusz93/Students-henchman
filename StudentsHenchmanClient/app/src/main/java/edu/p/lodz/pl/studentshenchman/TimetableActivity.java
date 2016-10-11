c2cd9ae406e9fc4611f369d7b73f1dd17c767ecc
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-11 22:25:09 +0200

    Poprawka layoutu

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/TimetableActivity.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/TimetableActivity.java
@@ -3,12 +3,12 @@ package edu.p.lodz.pl.studentshenchman;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
+
 
 public class TimetableActivity extends AppCompatActivity {
 
     ViewPager viewPager;
-    TabLayout tabLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +16,12 @@ public class TimetableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timetable);
 
         viewPager = (ViewPager) findViewById(R.id.container);
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
+
 
         if (viewPager != null) {
             DayPageAdapter dayPageAdapter = new DayPageAdapter(getSupportFragmentManager(), getApplicationContext());
             viewPager.setAdapter(dayPageAdapter);
 
-            if (tabLayout != null)
-            {
-                tabLayout.setupWithViewPager(viewPager);
-            }
         }
     }
 }
