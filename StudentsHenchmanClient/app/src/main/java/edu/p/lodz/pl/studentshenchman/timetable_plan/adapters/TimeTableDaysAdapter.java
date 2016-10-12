ecfdf4dc0b70285cfb00206f75a92fc69560a38f
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-12 22:19:08 +0200

    TimeTableDaysAdapter

---
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/adapters/TimeTableDaysAdapter.java
@@ -0,0 +1,41 @@
+package edu.p.lodz.pl.studentshenchman.timetable_plan.adapters;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+import edu.p.lodz.pl.studentshenchman.timetable_plan.fragments.DayFragment;
+
+/**
+ * Created by Michał on 2016-10-12.
+ */
+
+public class TimeTableDaysAdapter extends FragmentPagerAdapter {
+
+    private Context mContext;
+    private List<Fragment> mFragments;
+
+    public TimeTableDaysAdapter(Context context, FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        mContext = context;
+        mFragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragments.get(position).getArguments().getString(DayFragment.TAB_NAME);
+    }
+}
