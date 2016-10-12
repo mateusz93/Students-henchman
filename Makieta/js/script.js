f0e3d9011eba428b01116ba2fe23c87cf7cc563f
Piotr Ogrodowicz <ogrodowicz.piotr@gmail.com>
2016-10-12 22:40:00 +0200

    Makieta - Odnosniki do podstron na buttonach

---
+++ Makieta/js/script.js
@@ -0,0 +1,12 @@
+var sites = {
+    main: 'mainActivity.html',
+    settings: 'settingsActivity.html',
+    import: 'readDataActivity.html',
+    export: 'saveDataActivity',
+    timetable: 'timetableActivity',
+    
+};
+
+function gotoView(view) {
+    window.location.href = view;
+}
