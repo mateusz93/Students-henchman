3b7139efa89bdf2ff181c6a5b793aa6bdd3576c4
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-12 19:17:08 +0200

    TimeTableActivity i TimetableDaysFragment

---
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/fragments/TimetableDaysFragment.java
@@ -0,0 +1,10 @@
+package edu.p.lodz.pl.studentshenchman.timetable_plan.fragments;
+
+import edu.p.lodz.pl.studentshenchman.abstract_ui.StudentShenchmanMainFragment;
+
+/**
+ * Created by Michał on 2016-10-12.
+ */
+
+public class TimetableDaysFragment extends StudentShenchmanMainFragment {
+}
