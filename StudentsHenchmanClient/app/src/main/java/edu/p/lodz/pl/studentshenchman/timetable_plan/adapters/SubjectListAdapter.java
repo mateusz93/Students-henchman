3f771b2dfc6b18766204efa0df0c6bf9bc2868d8
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-13 22:34:37 +0200

    Poprawki w SubjectListAdapterze

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/adapters/SubjectListAdapter.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/adapters/SubjectListAdapter.java
@@ -24,10 +24,12 @@ public class SubjectListAdapter extends RecyclerView.Adapter<SubjectListAdapter.
     private static final String TAG = SubjectListAdapter.class.getName();
 
     private Context mContext;
+    private LayoutInflater mInflater;
     private OnItemClickListener mItemClickListener;
 
     public SubjectListAdapter(Context context) {
         this.mContext = context;
+        this.mInflater = LayoutInflater.from(mContext);
     }
 
     @Override
@@ -41,16 +43,16 @@ public class SubjectListAdapter extends RecyclerView.Adapter<SubjectListAdapter.
         holder.subjectName.setText("Analiza Matematyczna");
         holder.time.setText("8:15 - 9:45");
         holder.place.setText("Budynek xxx sala 104");
-        Picasso.with(mContext).load(R.color.colorPrimary).into(holder.placeImage);
+        Picasso.with(mContext).load(R.drawable.bg).into(holder.subjectImage);
 
-        Bitmap photo = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.nav);
+       // Bitmap photo = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.bg);
 
-        Palette.generateAsync(photo, new Palette.PaletteAsyncListener() {
+       /* Palette.generateAsync(photo, new Palette.PaletteAsyncListener() {
             public void onGenerated(Palette palette) {
                 int mutedLight = palette.getMutedColor(mContext.getResources().getColor(android.R.color.black));
-                holder.placeNameHolder.setBackgroundColor(mutedLight);
+                holder.subjectInfoHolder.setBackgroundColor(mutedLight);
             }
-        });
+        });*/
 
     }
 
@@ -58,6 +60,40 @@ public class SubjectListAdapter extends RecyclerView.Adapter<SubjectListAdapter.
     public int getItemCount() {
         return 5;
     }
+
+
+    @Override
+    public long getItemId(int position) {
+        return 3;
+    }
+
+   /* @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder;
+
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = mInflater.inflate(R.layout.subject_item_list, parent, false);
+            holder.mainHolder = (LinearLayout) convertView.findViewById(R.id.main_holder);
+            holder.subjectName = (TextView) convertView.findViewById(R.id.subject_name);
+            holder.time = (TextView) convertView.findViewById(R.id.time);
+            holder.place = (TextView) convertView.findViewById(R.id.place);
+            holder.subjectInfoHolder = (LinearLayout) convertView.findViewById(R.id.subject_info_holder);
+            holder.subjectImage = (ImageView) convertView.findViewById(R.id.subject_image);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.subjectName.setText("Analiza Matematyczna");
+        holder.time.setText("8:15 - 9:45");
+        holder.place.setText("Budynek xxx sala 104");
+        Picasso.with(mContext).load(R.drawable.bg).into(holder.subjectImage);
+        holder.subjectInfoHolder.setBackgroundColor(mContext.getColor(android.R.color.black));
+        return convertView;
+
+    }*/
 
     /*@Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
@@ -70,22 +106,22 @@ public class SubjectListAdapter extends RecyclerView.Adapter<SubjectListAdapter.
     }*/
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        public LinearLayout placeHolder;
-        public LinearLayout placeNameHolder;
+        public LinearLayout mainHolder;
+        public LinearLayout subjectInfoHolder;
         public TextView subjectName;
         public TextView time;
         public TextView place;
-        public ImageView placeImage;
+        public ImageView subjectImage;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            placeHolder = (LinearLayout) itemView.findViewById(R.id.mainHolder);
+            mainHolder = (LinearLayout) itemView.findViewById(R.id.main_holder);
             subjectName = (TextView) itemView.findViewById(R.id.subject_name);
             time = (TextView) itemView.findViewById(R.id.time);
             place = (TextView) itemView.findViewById(R.id.place);
-            placeNameHolder = (LinearLayout) itemView.findViewById(R.id.placeNameHolder);
-            placeImage = (ImageView) itemView.findViewById(R.id.placeImage);
-            // placeHolder.setOnClickListener(this);
+            subjectInfoHolder = (LinearLayout) itemView.findViewById(R.id.subject_info_holder);
+            subjectImage = (ImageView) itemView.findViewById(R.id.subject_image);
+            mainHolder.setOnClickListener(this);
         }
 
         @Override
@@ -101,4 +137,5 @@ public class SubjectListAdapter extends RecyclerView.Adapter<SubjectListAdapter.
     public void setOnItemClickListener(final OnItemClickListener mItemClickListener) {
         this.mItemClickListener = mItemClickListener;
     }
+
 }
