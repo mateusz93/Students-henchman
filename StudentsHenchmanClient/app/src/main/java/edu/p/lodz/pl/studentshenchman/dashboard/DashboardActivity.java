81c1e097b3a927187ab6e3828efd8326018b0b4c
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-19 23:00:43 +0200

    Dashboard - widok

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/dashboard/DashboardActivity.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/dashboard/DashboardActivity.java
@@ -2,30 +2,41 @@ package edu.p.lodz.pl.studentshenchman.dashboard;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
-import edu.p.lodz.pl.studentshenchman.QRCodeScanActivity;
 import edu.p.lodz.pl.studentshenchman.R;
-import edu.p.lodz.pl.studentshenchman.SettingsActivity;
 import edu.p.lodz.pl.studentshenchman.abstract_ui.StudentShenchmanMainActivity;
 import edu.p.lodz.pl.studentshenchman.database.DatabaseHelper;
+import edu.p.lodz.pl.studentshenchman.qr_scanner.QRCodeScanActivity;
+import edu.p.lodz.pl.studentshenchman.settings.SettingsActivity;
 import edu.p.lodz.pl.studentshenchman.timetable_plan.activity.TimetableActivity;
 
 public class DashboardActivity extends StudentShenchmanMainActivity {
     private static final String TAG = DashboardActivity.class.getName();
 
-    Toolbar toolbar;
+    private TextView mLessonName;
+    private TextView mTeacher;
+    private TextView mBuilding;
+    private TextView mRoom;
+    private TextView mLessonTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        /*toolbar = (Toolbar) findViewById(R.id.tool_bar);
-        prepareToolbar();*/
+        mLessonName = (TextView) findViewById(R.id.lesson_name);
+        mTeacher = (TextView) findViewById(R.id.widget_teacher_name);
+        mBuilding = (TextView) findViewById(R.id.widget_building_name);
+        mRoom = (TextView) findViewById(R.id.widget_room_name);
+        mLessonTime = (TextView) findViewById(R.id.widget_lesson_time);
+
+        mLessonName.setText("Projektowanie aplikacji internetowych");
+        mTeacher.setText("Dr. inz Rafal Kielbik");
+        mBuilding.setText("CTI");
+        mRoom.setText("303");
+        mLessonTime.setText("8:15 - 10:00");
 
         DatabaseHelper.getInstance(getApplicationContext());
 
@@ -50,34 +61,6 @@ public class DashboardActivity extends StudentShenchmanMainActivity {
         );
     }
 
-    public void prepareToolbar() {
-
-        setSupportActionBar(toolbar);
-        if (null != getSupportActionBar()) {
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-            getSupportActionBar().setDisplayUseLogoEnabled(true);
-            getSupportActionBar().setHomeButtonEnabled(true);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                goToSettings();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
     private void goToTimetable() {
         Intent intent = new Intent(DashboardActivity.this, TimetableActivity.class);
         finish();
@@ -89,7 +72,6 @@ public class DashboardActivity extends StudentShenchmanMainActivity {
         finish();
         startActivity(intent);
     }
-
 
     private void goToScanQRCode() {
         Intent intent = new Intent(DashboardActivity.this, QRCodeScanActivity.class);
--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/activity/TimetableActivity.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/activity/TimetableActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
-import edu.p.lodz.pl.studentshenchman.DashboardActivity;
+import edu.p.lodz.pl.studentshenchman.dashboard.DashboardActivity;
 import edu.p.lodz.pl.studentshenchman.R;
 import edu.p.lodz.pl.studentshenchman.abstract_ui.StudentShenchmanMainActivity;
 
