507cf32d5ae4e5d22939e2b1b01fb87f3d64d94a
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-21 15:18:23 +0200

    Modyfikacja layoutu SettingsActivity

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/settings/SettingsActivity.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/settings/SettingsActivity.java
@@ -3,6 +3,7 @@ package edu.p.lodz.pl.studentshenchman.settings;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
@@ -12,6 +13,10 @@ import edu.p.lodz.pl.studentshenchman.R;
 import edu.p.lodz.pl.studentshenchman.abstract_ui.StudentShenchmanMainActivity;
 import edu.p.lodz.pl.studentshenchman.dashboard.DashboardActivity;
 import edu.p.lodz.pl.studentshenchman.settings.adapters.DepartmentAdapter;
+import edu.p.lodz.pl.studentshenchman.settings.adapters.FieldAdapter;
+import edu.p.lodz.pl.studentshenchman.settings.adapters.KindAdapter;
+import edu.p.lodz.pl.studentshenchman.settings.adapters.SpecializationAdapter;
+import edu.p.lodz.pl.studentshenchman.settings.adapters.TypeAdapter;
 
 public class SettingsActivity extends StudentShenchmanMainActivity {
 	private static final String TAG = SettingsActivity.class.getName();
@@ -19,10 +24,12 @@ public class SettingsActivity extends StudentShenchmanMainActivity {
 	private Toolbar toolbar;
 	private LinearLayout mDepartmentLinear;
 	private LinearLayout mFieldLinear;
-	private LinearLayout mSpecializationLayout;
+	private LinearLayout mSpecializationLinear;
 	private Spinner mDepartmentSpinner;
 	private Spinner mFieldSpinner;
 	private Spinner mSpecializationSpinner;
+	private Spinner mTypeSpinner;
+	private Spinner mKindSpinner;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +40,27 @@ public class SettingsActivity extends StudentShenchmanMainActivity {
 
 		mDepartmentLinear = (LinearLayout) findViewById(R.id.department_content);
 		mFieldLinear = (LinearLayout) findViewById(R.id.field_content);
-		mSpecializationLayout = (LinearLayout) findViewById(R.id.specialization_content);
+		mSpecializationLinear = (LinearLayout) findViewById(R.id.specialization_content);
 
 		mDepartmentSpinner = (Spinner) findViewById(R.id.department_spinner);
 		mDepartmentSpinner.setAdapter(new DepartmentAdapter(getApplicationContext(), new ArrayList<>()));
+
+		mFieldSpinner = (Spinner) findViewById(R.id.field_spinner);
+		mFieldSpinner.setAdapter(new FieldAdapter(getApplicationContext(), new ArrayList<Object>()));
+		mFieldLinear.setVisibility(View.VISIBLE);
+
+		mSpecializationSpinner = (Spinner) findViewById(R.id.specialization_spinner);
+		mSpecializationSpinner.setAdapter(new SpecializationAdapter(getApplicationContext(), new ArrayList<Object>()));
+		mSpecializationLinear.setVisibility(View.VISIBLE);
+
+		mTypeSpinner = (Spinner) findViewById(R.id.type_spinner);
+		mTypeSpinner.setAdapter(new TypeAdapter(getApplicationContext(), new ArrayList<Object>()));
+
+
+		mKindSpinner = (Spinner) findViewById(R.id.kind_spinner);
+		mKindSpinner.setAdapter(new KindAdapter(getApplicationContext(), new ArrayList<Object>()));
+
+
 	}
 
 	private void prepareToolbar() {
