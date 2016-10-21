42ab2cebb38f7b71168610c5fd38ceae81924fcc
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-21 12:19:24 +0200

    SettingsActivity poprawki layoutu i dodanie adaptera dla wydziałów

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/settings/SettingsActivity.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/settings/SettingsActivity.java
@@ -3,15 +3,26 @@ package edu.p.lodz.pl.studentshenchman.settings;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 import edu.p.lodz.pl.studentshenchman.R;
 import edu.p.lodz.pl.studentshenchman.abstract_ui.StudentShenchmanMainActivity;
 import edu.p.lodz.pl.studentshenchman.dashboard.DashboardActivity;
+import edu.p.lodz.pl.studentshenchman.timetable_plan.adapters.DepartmentAdapter;
 
 public class SettingsActivity extends StudentShenchmanMainActivity {
 	private static final String TAG = SettingsActivity.class.getName();
 
 	private Toolbar toolbar;
+	private LinearLayout mDepartmentLinear;
+	private LinearLayout mFieldLinear;
+	private LinearLayout mSpecializationLayout;
+	private Spinner mDepartmentSpinner;
+	private Spinner mFieldSpinner;
+	private Spinner mSpecializationSpinner;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,13 @@ public class SettingsActivity extends StudentShenchmanMainActivity {
 		setContentView(R.layout.activity_settings);
 		toolbar = (Toolbar) findViewById(R.id.tool_bar);
 		prepareToolbar();
+
+		mDepartmentLinear = (LinearLayout) findViewById(R.id.department_content);
+		mFieldLinear = (LinearLayout) findViewById(R.id.field_content);
+		mSpecializationLayout = (LinearLayout) findViewById(R.id.specialization_content);
+
+		mDepartmentSpinner = (Spinner) findViewById(R.id.department_spinner);
+		mDepartmentSpinner.setAdapter(new DepartmentAdapter(getApplicationContext(), new ArrayList<>()));
 	}
 
 	private void prepareToolbar() {
