f04b54861d6e7dc73cccd0672d5740e6fd369f83
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-21 15:55:19 +0200

    Dodany LoginActivity LoginActivity

---
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/login/activities/LoginActivity.java
@@ -0,0 +1,49 @@
+package edu.p.lodz.pl.studentshenchman.login.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import edu.p.lodz.pl.studentshenchman.R;
+import edu.p.lodz.pl.studentshenchman.dashboard.DashboardActivity;
+
+/**
+ * @author Michal Warcholinski
+ */
+public class LoginActivity extends AppCompatActivity {
+	private static final String TAG = LoginActivity.class.getName();
+
+	private Button mLoginButton;
+	private Button mClearButton;
+	private EditText mLogin;
+	private EditText mPassword;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_login);
+
+		mLogin = (EditText) findViewById(R.id.login);
+		mPassword = (EditText) findViewById(R.id.password);
+
+		mLoginButton = (Button) findViewById(R.id.login_button);
+		mLoginButton.setOnClickListener((view) ->
+						goToDashboard()
+		);
+
+		mClearButton = (Button) findViewById(R.id.clear_button);
+		mClearButton.setOnClickListener((view) -> {
+			mLogin.setText("");
+			mPassword.setText("");
+		});
+	}
+
+	private void goToDashboard() {
+		Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+		finish();
+		startActivity(intent);
+	}
+}
