b592220a835a814a8c33993e912156a3b8b9946c
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-21 10:54:00 +0200

    Ustawianie stylu dividera

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/qr_scanner/SimpleScanner.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/qr_scanner/SimpleScanner.java
@@ -1,62 +1,78 @@
 package edu.p.lodz.pl.studentshenchman.qr_scanner;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.widget.Toast;
-import com.google.zxing.Result;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.zxing.Result;
+
 import java.io.IOException;
 
-import edu.p.lodz.pl.studentshenchman.utils.AlertDialogActivity;
 import edu.p.lodz.pl.studentshenchman.classes.Information;
+import edu.p.lodz.pl.studentshenchman.dashboard.DashboardActivity;
+import edu.p.lodz.pl.studentshenchman.utils.AlertDialogActivity;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-import static edu.p.lodz.pl.studentshenchman.workers.IdleWorker.TAG;
-
 public class SimpleScanner extends FragmentActivity implements ZXingScannerView.ResultHandler {
-    private ZXingScannerView mScannerView;
-    FragmentManager fm = getSupportFragmentManager();
+	private static final String TAG = SimpleScanner.class.getName();
 
-    @Override
-    public void onCreate(Bundle state) {
-        super.onCreate(state);
-        mScannerView = new ZXingScannerView(this);
-        setContentView(mScannerView);
-    }
+	private ZXingScannerView mScannerView;
+	FragmentManager fm = getSupportFragmentManager();
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.v(TAG, "onResume");
-        mScannerView.setResultHandler(this);
-        mScannerView.startCamera();
-    }
+	@Override
+	public void onCreate(Bundle state) {
+		super.onCreate(state);
+		mScannerView = new ZXingScannerView(this);
+		setContentView(mScannerView);
+	}
 
-    @Override
-    public void onPause() {
-        Log.v(TAG, "onPause");
-        super.onPause();
-        mScannerView.stopCamera();
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.v(TAG, "onResume");
+		mScannerView.setResultHandler(this);
+		mScannerView.startCamera();
+	}
 
-    @Override
-    public void handleResult(Result rawResult) {
-        Log.v(TAG, rawResult.getText());
-        Log.v(TAG, rawResult.getBarcodeFormat().toString());
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            Information information = mapper.readValue(rawResult.getText(), Information.class);
-            String formattedText = Information.getFormattedFields(information);
-            AlertDialogActivity alertDialogActivity = new AlertDialogActivity();
-            Bundle args = new Bundle();
-            args.putString("text",formattedText);
-            alertDialogActivity.setArguments(args);
-            alertDialogActivity.show(fm, formattedText);
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), "Nieobsługiwany komunikat!", Toast.LENGTH_SHORT).show();
-        }
-        mScannerView.resumeCameraPreview(this);
-    }
+	@Override
+	public void onPause() {
+		Log.v(TAG, "onPause");
+		super.onPause();
+		mScannerView.stopCamera();
+	}
+
+	@Override
+	public void handleResult(Result rawResult) {
+		Log.v(TAG, rawResult.getText());
+		Log.v(TAG, rawResult.getBarcodeFormat().toString());
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			Information information = mapper.readValue(rawResult.getText(), Information.class);
+			String formattedText = Information.getFormattedFields(information);
+			AlertDialogActivity alertDialogActivity = new AlertDialogActivity();
+			Bundle args = new Bundle();
+			args.putString("text", formattedText);
+			alertDialogActivity.setArguments(args);
+			alertDialogActivity.show(fm, formattedText);
+		} catch (IOException e) {
+			Log.i(TAG, e.toString());
+			Toast.makeText(getApplicationContext(), "Nieobsługiwany komunikat!", Toast.LENGTH_SHORT).show();
+		}
+		mScannerView.resumeCameraPreview(this);
+	}
+
+	@Override
+	public void onBackPressed() {
+		goToDashboard();
+	}
+
+	private void goToDashboard() {
+		Intent previousActivity = new Intent(SimpleScanner.this, DashboardActivity.class);
+		finish();
+		startActivity(previousActivity);
+	}
 }
