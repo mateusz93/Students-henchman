61045cc68e3e0585fc0ad3a0c473ed1ba6149514
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-23 13:52:40 +0200

    Dodane tabele do DatabaseHelpera

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/database/DatabaseHelper.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/database/DatabaseHelper.java
@@ -8,8 +8,13 @@ import android.util.Log;
 import java.util.HashMap;
 import java.util.Map;
 
+import edu.p.lodz.pl.studentshenchman.database.models.Department;
+import edu.p.lodz.pl.studentshenchman.database.models.Field;
+import edu.p.lodz.pl.studentshenchman.database.models.Kind;
 import edu.p.lodz.pl.studentshenchman.database.models.Room;
+import edu.p.lodz.pl.studentshenchman.database.models.Specialization;
 import edu.p.lodz.pl.studentshenchman.database.models.Teacher;
+import edu.p.lodz.pl.studentshenchman.database.models.Type;
 import edu.p.lodz.pl.studentshenchman.database.models.User;
 
 
@@ -36,6 +41,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             put(User.TABLE_NAME, createTableQuery(User.TABLE_NAME, User.COLUMN_NAMES, User.COLUMN_TYPES));
             put(Teacher.TABLE_NAME, createTableQuery(Teacher.TABLE_NAME, Teacher.COLUMN_NAMES, Teacher.COLUMN_TYPES));
             put(Room.TABLE_NAME, createTableQuery(Room.TABLE_NAME, Room.COLUMN_NAMES, Room.COLUMN_TYPES));
+            put(Department.TABLE_NAME, createTableQuery(Department.TABLE_NAME, Department.COLUMN_NAMES, Department.COLUMN_TYPES));
+            put(Field.TABLE_NAME, createTableQuery(Field.TABLE_NAME, Field.COLUMN_NAMES, Field.COLUMN_TYPES));
+            put(Specialization.TABLE_NAME, createTableQuery(Specialization.TABLE_NAME, Specialization.COLUMN_NAMES, Specialization.COLUMN_TYPES));
+            put(Type.TABLE_NAME, createTableQuery(Type.TABLE_NAME, Type.COLUMN_NAMES, Type.COLUMN_TYPES));
+            put(Kind.TABLE_NAME, createTableQuery(Kind.TABLE_NAME, Kind.COLUMN_NAMES, Kind.COLUMN_TYPES));
         }
     };
 
@@ -93,5 +103,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return true;
         return false;
     }
+
 
 }
