001b3b03d3340d2949d9b3a6205f7313bc2857c5
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-24 21:07:32 +0200

    Dodany drawer do dashboardu

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/dashboard/DashboardActivity.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/dashboard/DashboardActivity.java
@@ -10,12 +10,21 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.p.lodz.pl.studentshenchman.R;
 import edu.p.lodz.pl.studentshenchman.abstract_ui.StudentShenchmanMainActivity;
+import edu.p.lodz.pl.studentshenchman.dashboard.adapters.DrawerListAdapter;
+import edu.p.lodz.pl.studentshenchman.dashboard.drawer_data.DrawerItem;
 import edu.p.lodz.pl.studentshenchman.database.DatabaseHelper;
 import edu.p.lodz.pl.studentshenchman.database.models.Department;
 import edu.p.lodz.pl.studentshenchman.database.models.Field;
@@ -30,22 +39,33 @@ public class DashboardActivity extends StudentShenchmanMainActivity {
     private static final String TAG = DashboardActivity.class.getName();
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
 
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
     private TextView mLessonName;
     private TextView mTeacher;
     private TextView mBuilding;
     private TextView mRoom;
     private TextView mLessonTime;
 
+    private DrawerListAdapter mDrawerAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.drawer_list);
         mLessonName = (TextView) findViewById(R.id.item_lesson_name);
         mTeacher = (TextView) findViewById(R.id.item_teacher_name);
         mBuilding = (TextView) findViewById(R.id.item_building_name);
         mRoom = (TextView) findViewById(R.id.item_room_name);
         mLessonTime = (TextView) findViewById(R.id.item_lesson_time);
+
+        mDrawerAdapter = new DrawerListAdapter(getApplicationContext(), getDrawerItemList());
+        mDrawerList.setAdapter(mDrawerAdapter);
+        mDrawerList.setOnItemClickListener(new DrawerOnItemClickListener());
 
         mLessonName.setText("Projektowanie aplikacji internetowych");
         mTeacher.setText("Dr. inz Rafal Kielbik");
@@ -69,7 +89,6 @@ public class DashboardActivity extends StudentShenchmanMainActivity {
 
         buttonTimetable.setOnClickListener((view) ->
                 goToTimetable()
-
         );
 
         ImageButton settingsButton = (ImageButton) findViewById(R.id.settings_icon);
@@ -82,8 +101,18 @@ public class DashboardActivity extends StudentShenchmanMainActivity {
 
         scanQRCode.setOnClickListener((view) ->
                 goToScanQRCode()
-
         );
+    }
+
+    private List<DrawerItem> getDrawerItemList() {
+        List<DrawerItem> drawerItems = new ArrayList<>();
+        drawerItems.add(new DrawerItem(getString(R.string.drawer_settings_name), R.drawable.logo_mini));
+        drawerItems.add(new DrawerItem(getString(R.string.drawer_refresh_timetable_data), R.drawable.logo_mini));
+        drawerItems.add(new DrawerItem(getString(R.string.drawer_refresh_settings_data), R.drawable.logo_mini));
+        drawerItems.add(new DrawerItem(getString(R.string.drawer_about_app), R.drawable.logo_mini));
+        drawerItems.add(new DrawerItem(getString(R.string.drawer_logout_name), R.drawable.logo_mini));
+        return drawerItems;
+
     }
 
     private void saveTestDataIntoDatabase() {
@@ -222,6 +251,14 @@ public class DashboardActivity extends StudentShenchmanMainActivity {
                     Toast.makeText(this, "Brak uprawnień do aparatu!", Toast.LENGTH_SHORT).show();
                 }
             }
+        }
+    }
+
+
+    private class DrawerOnItemClickListener implements android.widget.AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(getApplicationContext(), "drawer", Toast.LENGTH_LONG).show();
         }
     }
 }
