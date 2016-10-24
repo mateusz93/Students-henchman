2dce3785788c511361512109874f23895ff0a560
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-24 21:11:16 +0200

    Dodanie DrawerAdapter oraz layoutu dla ItemDrawera

---
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/dashboard/drawer_data/DrawerItem.java
@@ -0,0 +1,32 @@
+package edu.p.lodz.pl.studentshenchman.dashboard.drawer_data;
+
+/**
+ * Created by Michał on 2016-10-24.
+ */
+
+public class DrawerItem {
+
+    private String name;
+    private int icon;
+
+    public DrawerItem(String name, int icon) {
+        this.name = name;
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+}
