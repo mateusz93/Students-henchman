c2791f974652d491517eec540198cc03d7a33c34
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-25 22:51:45 +0200

    Dodanie zaleznosci blokowania uzytkownika oraz pokazywania progres baru w zaleznosci od tego co wybral uzytkownik

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/fragments/EditTimeTableDialogFragment.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/fragments/EditTimeTableDialogFragment.java
@@ -13,6 +13,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import edu.p.lodz.pl.studentshenchman.R;
+import edu.p.lodz.pl.studentshenchman.timetable_plan.activity.TimetableActivity;
 import edu.p.lodz.pl.studentshenchman.timetable_plan.adapters.EditTimeTableListAdapter;
 
 /**
@@ -22,6 +23,8 @@ import edu.p.lodz.pl.studentshenchman.timetable_plan.adapters.EditTimeTableListA
 public class EditTimeTableDialogFragment extends DialogFragment {
 
 	private static final String TITLE = "title";
+	private static final String WORKER_RUNNING = "worker_running";
+	private static final String CANCELABLE = "cancelable";
 	private TextView mTitle;
 	private ListView mList;
 	private LinearLayout mButtonsLinear;
@@ -29,6 +32,10 @@ public class EditTimeTableDialogFragment extends DialogFragment {
 	private Button mDeleteButton;
 	private LinearLayout mProgressBarContent;
 	private EditTimeTableListAdapter mEditListAdapter;
+	private EditedCoursesDialogInterface callback;
+
+	private boolean mIsWorkerRunning;
+	private boolean mIsCancelable;
 
 	public EditTimeTableDialogFragment() {
 	}
@@ -44,10 +51,22 @@ public class EditTimeTableDialogFragment extends DialogFragment {
 		return editTimeTableDialogFragment;
 	}
 
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (null != savedInstanceState) {
+			mIsWorkerRunning = savedInstanceState.getBoolean(WORKER_RUNNING, false);
+			mIsCancelable = savedInstanceState.getBoolean(CANCELABLE, false);
+		}
+		callback = (TimetableActivity) getActivity();
+	}
+
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		getDialog().requestWindowFeature(STYLE_NO_TITLE);
 		return inflater.inflate(R.layout.edit_timetable_dialog_fragment, container);
+
 	}
 
 	@Override
@@ -70,13 +89,35 @@ public class EditTimeTableDialogFragment extends DialogFragment {
 		mTitle.setText(title);
 	}
 
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		outState.putBoolean(WORKER_RUNNING, mIsWorkerRunning);
+		outState.putBoolean(CANCELABLE, mIsCancelable);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (mIsWorkerRunning) {
+			showProgressBar();
+		}
+		setCancelable(mIsCancelable);
+	}
+
+	private void showProgressBar() {
+		mButtonsLinear.setVisibility(View.GONE);
+		mList.setVisibility(View.GONE);
+		mProgressBarContent.setVisibility(View.VISIBLE);
+	}
+
 
 	private class EditOnClickListener implements View.OnClickListener {
 		@Override
 		public void onClick(View v) {
-			mButtonsLinear.setVisibility(View.GONE);
-			mList.setVisibility(View.GONE);
-			mProgressBarContent.setVisibility(View.VISIBLE);
+			mIsCancelable = false;
+			setCancelable(mIsCancelable);
+			mIsWorkerRunning = true;
+			showProgressBar();
 			//mList.setVisibility(View.VISIBLE);
 			// tutaj trzeba wywolac workera ktory pobierze odpowiednie dane a w callbacku ustawi adapter na liscie z pobranymi danymi
 			// i pokaze odpowiedni widok
@@ -86,8 +127,11 @@ public class EditTimeTableDialogFragment extends DialogFragment {
 	private class DeleteOnClickListener implements View.OnClickListener {
 		@Override
 		public void onClick(View v) {
+			mIsCancelable = false;
+			setCancelable(mIsCancelable);
 			// trzeba wywaloc np callback albo zaimplementowac jakis interfejs i przekazac id wybranych zajec do usuniecia oraz
 			// ustawic flage zmiany danych na true
+			callback.courseToDeleteSelected(5);
 			dismiss();
 		}
 	}
@@ -95,8 +139,15 @@ public class EditTimeTableDialogFragment extends DialogFragment {
 	private class EditItemListOnItemClickListener implements android.widget.AdapterView.OnItemClickListener {
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+			callback.courseToSwapSelected(id);
 			dismiss();
 		}
 	}
+
+	public interface EditedCoursesDialogInterface {
+		void courseToSwapSelected(long id);
+
+		void courseToDeleteSelected(long id);
+	}
+
 }
