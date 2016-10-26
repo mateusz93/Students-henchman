14a8eb33abc28c54f5b38a399cc3e05cbf95d45c
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-26 14:46:12 +0200

    Dodanie buttona cancel

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/fragments/EditTimeTableDialogFragment.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/fragments/EditTimeTableDialogFragment.java
@@ -30,6 +30,7 @@ public class EditTimeTableDialogFragment extends DialogFragment {
 	private LinearLayout mButtonsLinear;
 	private Button mEditButton;
 	private Button mDeleteButton;
+	private Button mCancelButton;
 	private LinearLayout mProgressBarContent;
 	private EditTimeTableListAdapter mEditListAdapter;
 	private EditedCoursesDialogInterface callback;
@@ -79,6 +80,8 @@ public class EditTimeTableDialogFragment extends DialogFragment {
 		mEditButton.setOnClickListener(new EditOnClickListener());
 		mDeleteButton = (Button) view.findViewById(R.id.delete_button);
 		mDeleteButton.setOnClickListener(new DeleteOnClickListener());
+		mCancelButton = (Button) view.findViewById(R.id.cancel_button);
+		mCancelButton.setOnClickListener(new CancelOnClickListener());
 		mProgressBarContent = (LinearLayout) view.findViewById(R.id.progress_bar_content);
 		mList = (ListView) view.findViewById(R.id.list);
 		mEditListAdapter = new EditTimeTableListAdapter(getContext());
@@ -150,4 +153,10 @@ public class EditTimeTableDialogFragment extends DialogFragment {
 		void courseToDeleteSelected(long id);
 	}
 
+	private class CancelOnClickListener implements View.OnClickListener {
+		@Override
+		public void onClick(View v) {
+			dismiss();
+		}
+	}
 }
