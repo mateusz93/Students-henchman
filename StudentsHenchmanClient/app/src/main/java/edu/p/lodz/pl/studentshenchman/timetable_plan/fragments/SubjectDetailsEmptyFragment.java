fe4b003ebb95b040323f4f4be193a94f5450ae82
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-10-27 13:38:16 +0200

    Dodanie pustego fragmentu jezeli zadne zajecie nie zostalo jeszcze wybrane

---
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/fragments/SubjectDetailsEmptyFragment.java
@@ -0,0 +1,10 @@
+package edu.p.lodz.pl.studentshenchman.timetable_plan.fragments;
+
+import edu.p.lodz.pl.studentshenchman.abstract_ui.StudentShenchmanMainFragment;
+
+/**
+ * @author Michal Warcholinski
+ */
+public class SubjectDetailsEmptyFragment extends StudentShenchmanMainFragment {
+
+}
