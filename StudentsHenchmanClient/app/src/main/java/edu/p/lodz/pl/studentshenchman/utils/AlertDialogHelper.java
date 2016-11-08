7e8f164379778689bfa8030b682d0f59a2941082
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-08 15:38:53 +0100

    Dodane klasy pomocnicze do budowania alertow dla uzytkownika.
    Do rozwiazania problem znikajacych alertow po obrocie.
    Klasa, ktora oczekuje callbacka od uzytkownika w celu wykorzystania go musi zaimplementowac AlertDialogCallback
    Domyslnie, np. przy pokazywaniu bledu, nie potrzebny bedzie customowy callback

---
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/utils/AlertDialogHelper.java
@@ -0,0 +1,9 @@
+package edu.p.lodz.pl.studentshenchman.utils;
+
+/**
+ * Created by Michał on 2016-11-06.
+ */
+
+public class AlertDialogHelper {
+
+}
---
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/utils/interfaces/AlertDialogCallback.java
@@ -0,0 +1,13 @@
+package edu.p.lodz.pl.studentshenchman.utils.interfaces;
+
+/**
+ * Created by Michał on 2016-11-06.
+ */
+
+public interface AlertDialogCallback {
+	void positiveCallback();
+
+	void negativeCallback();
+
+	//void neutralCallback();
+}
