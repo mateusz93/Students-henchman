d68831ffd77b0711cd875a416aa6a8b79f3e7dbe
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-11 19:43:44 +0100

    Poprawa literowki

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/settings/adapters/DegreeAdapter.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/settings/adapters/DegreeAdapter.java
@@ -33,7 +33,7 @@ public class DegreeAdapter extends BaseAdapter {
 		mValues.add(new Degree(mContext.getString(R.string.spinner_option_choose), 0L));
 		mValues.add(new Degree(mContext.getString(R.string.settings_degree_spinner_value_label, 1), 1L));
 		mValues.add(new Degree(mContext.getString(R.string.settings_degree_spinner_value_label, 2), 2L));
-		mValues.add(new Degree(mContext.getString(R.string.settings_degree_spinner_value_label, 2), 2L));
+		mValues.add(new Degree(mContext.getString(R.string.settings_degree_spinner_value_label, 2), 3L));
 	}
 
 
