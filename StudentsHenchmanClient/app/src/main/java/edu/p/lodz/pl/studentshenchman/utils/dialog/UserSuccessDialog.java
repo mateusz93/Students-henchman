bcfa5e5671549edbfa2bc5f4e7143d9c6838e9ba
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-12 16:28:15 +0100

    Dodanie UserSuccesDialog

---
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/utils/dialog/UserSuccessDialog.java
@@ -0,0 +1,82 @@
+package edu.p.lodz.pl.studentshenchman.utils.dialog;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import edu.p.lodz.pl.studentshenchman.R;
+import edu.p.lodz.pl.studentshenchman.utils.animation.AnimationHelper;
+
+/**
+ * Created by Michał on 2016-11-12.
+ */
+
+public class UserSuccessDialog extends DialogFragment {
+	private static final String TAG = UserSuccessDialog.class.getName();
+
+	private static final String TITLE = TAG + ":title";
+	private static final String MESSAGE = TAG + "message";
+	private Button mOkButton;
+	private TextView mTitle;
+	private TextView mMessage;
+	private ImageView mIcon;
+
+	public static UserSuccessDialog getInstance(String title, String message) {
+		UserSuccessDialog dialog = new UserSuccessDialog();
+		Bundle bundle = new Bundle();
+		bundle.putString(TITLE, title);
+		bundle.putString(MESSAGE, message);
+		dialog.setArguments(bundle);
+		return dialog;
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		getDialog().requestWindowFeature(STYLE_NO_TITLE);
+		return inflater.inflate(R.layout.user_success_dialog_fragment, container);
+	}
+
+	@Override
+	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		mOkButton = (Button) view.findViewById(R.id.button);
+		mOkButton.setOnClickListener(new OkOnClickListener());
+
+		mIcon = (ImageView) view.findViewById(R.id.icon);
+		mTitle = (TextView) view.findViewById(R.id.title);
+		mMessage = (TextView) view.findViewById(R.id.message);
+
+		Bundle b = getArguments();
+		mTitle.setText(b.getString(TITLE));
+		mMessage.setText(b.getString(MESSAGE));
+		mIcon.setImageResource(R.drawable.about_app_icon);
+
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		final View decorView = getDialog()
+				.getWindow()
+				.getDecorView();
+
+		AnimationHelper.startFallAnimation(decorView);
+	}
+
+
+	private class OkOnClickListener implements View.OnClickListener {
+		@Override
+		public void onClick(View v) {
+			dismiss();
+		}
+	}
+}
--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/utils/dialog/factory/DialogFactory.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/utils/dialog/factory/DialogFactory.java
@@ -1,10 +1,12 @@
 package edu.p.lodz.pl.studentshenchman.utils.dialog.factory;
 
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 
 import edu.p.lodz.pl.studentshenchman.utils.dialog.DialogType;
 import edu.p.lodz.pl.studentshenchman.utils.dialog.UserErrorDialog;
 import edu.p.lodz.pl.studentshenchman.utils.dialog.UserInfoDialog;
+import edu.p.lodz.pl.studentshenchman.utils.dialog.UserSuccessDialog;
 
 /**
  * Created by Michał on 2016-11-12.
@@ -14,11 +16,14 @@ public class DialogFactory {
 	private static final String TAG = DialogFactory.class.getName();
 
 	public static DialogFragment produceDialog(String title, String message, DialogType dialogType) {
+		Log.i(TAG, "Tworzenie dialog fragmentu: tytul: " + title + " wiadomosc: " + message + " typ: " + dialogType.name());
 		switch (dialogType) {
 			case USER_INFO:
 				return UserInfoDialog.getInstance(title, message);
 			case USER_ERROR:
 				return UserErrorDialog.getInstance(title, message);
+			case USER_SUCCESS:
+				return UserSuccessDialog.getInstance(title, message);
 			default:
 				throw new IllegalArgumentException("Unknown dialog type");
 		}
