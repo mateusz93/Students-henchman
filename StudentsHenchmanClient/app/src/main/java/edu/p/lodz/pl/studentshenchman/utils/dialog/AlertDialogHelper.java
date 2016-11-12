a71605699b40312c6605337d159c78745c9c9761
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-12 16:22:07 +0100

    Dodanie do dialoghelpera metod do pokazywania dialogu

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/utils/dialog/AlertDialogHelper.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/utils/dialog/AlertDialogHelper.java
@@ -1,9 +1,31 @@
 package edu.p.lodz.pl.studentshenchman.utils.dialog;
 
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+
+import edu.p.lodz.pl.studentshenchman.utils.dialog.factory.DialogFactory;
+
 /**
  * Created by Michał on 2016-11-06.
  */
 
 public class AlertDialogHelper {
+	private static final String TAG = AlertDialogHelper.class.getName();
 
+	public static void showErrorDialog(FragmentManager fm, String title, String message) {
+		showDialog(fm, title, message, DialogType.USER_ERROR);
+	}
+
+	public static void showInfoDialog(FragmentManager fm, String title, String message) {
+		showDialog(fm, title, message, DialogType.USER_INFO);
+	}
+
+	public static void showSuccessDialog(FragmentManager fm, String title, String message) {
+		showDialog(fm, title, message, DialogType.USER_SUCCESS);
+	}
+
+	public static void showDialog(FragmentManager fm, String title, String message, DialogType dialogType) {
+		DialogFragment dialog = DialogFactory.produceDialog(title, message, dialogType);
+		dialog.show(fm, TAG);
+	}
 }
