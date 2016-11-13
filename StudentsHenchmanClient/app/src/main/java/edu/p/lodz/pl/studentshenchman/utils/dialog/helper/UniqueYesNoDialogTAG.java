9512efd9fffb9c8dd12e6a1fdc65907ec4d5f60a
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-13 12:43:14 +0100

    Refaktoryzacja klasy przechowującej tagi dla YesNo dialogu

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/utils/dialog/helper/UniqueYesNoDialogTAG.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/utils/dialog/helper/UniqueYesNoDialogTAG.java
@@ -4,8 +4,8 @@ package edu.p.lodz.pl.studentshenchman.utils.dialog.helper;
  * Created by Michał on 2016-11-13.
  */
 
-public class UniqueDialogTAG {
-	private static final String TAG = UniqueDialogTAG.class.getName();
+public class UniqueYesNoDialogTAG {
+	private static final String TAG = UniqueYesNoDialogTAG.class.getName();
 
 	public static final String DELETE_NOTE_TAG = TAG + ":delete_note_tag";
 	public static final String ADD_NOTE_TAG = TAG + ":add_note_tag";
