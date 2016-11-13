af1f8f819c31567c9bc40e193a5d3891c445561e
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-13 19:57:46 +0100

    Poprawa modelu Note

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/database/models/Note.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/database/models/Note.java
@@ -1,5 +1,6 @@
 package edu.p.lodz.pl.studentshenchman.database.models;
 
+import android.database.Cursor;
 import android.provider.BaseColumns;
 
 import java.util.HashMap;
@@ -15,6 +16,7 @@ public class Note implements BaseColumns {
 	public static final String TABLE_NAME = "note";
 
 	public static final String _ID = "_id";
+	public static final String COURSE_ID = "course_id";
 	public static final String CONTENT = "content";
 	public static final String ADDED_DATE = "added_date";
 
@@ -25,18 +27,79 @@ public class Note implements BaseColumns {
 		projectionColumns = new HashMap<>();
 		projectionColumns.put(_ID,
 				_ID);
+		projectionColumns.put(COURSE_ID,
+				COURSE_ID);
 		projectionColumns.put(CONTENT,
 				CONTENT);
 		projectionColumns.put(ADDED_DATE,
 				ADDED_DATE);
 	}
 
-	public static final String[] COLUMN_NAMES = {_ID, CONTENT, ADDED_DATE};
+	public static final String[] COLUMN_NAMES = {_ID, COURSE_ID, CONTENT, ADDED_DATE};
 
 
 	public static final String[] COLUMN_TYPES = {
-			"integer primary key autoincrement",    //BUILD_ID
+			"integer primary key autoincrement",     //NOTE_ID
+			"integer",                               //COURSE_ID
 			"text",                                  //CONTENT
 			"long",                                  //ADDED_DATE
 	};
+
+
+	private long id;
+	private long courseId;
+	private String content;
+	private long addedDate;
+
+	public Note() {
+	}
+
+	public Note(Cursor cursor) {
+		id = cursor.getLong(cursor.getColumnIndexOrThrow(Note._ID));
+		courseId = cursor.getLong(cursor.getColumnIndexOrThrow(Note.COURSE_ID));
+		content = cursor.getString(cursor.getColumnIndexOrThrow(Note.CONTENT));
+		addedDate = cursor.getLong(cursor.getColumnIndexOrThrow(Note.ADDED_DATE));
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(long courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public long getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(long addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Note{" +
+				"id=" + id +
+				", courseId=" + courseId +
+				", content='" + content + '\'' +
+				", addedDate=" + addedDate +
+				'}';
+	}
 }
