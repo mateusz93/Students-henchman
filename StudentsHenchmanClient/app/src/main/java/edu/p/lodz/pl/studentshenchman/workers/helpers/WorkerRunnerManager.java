7267bfd1f6cdbcab365a6dfc3c218afee179b78b
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-15 00:14:21 +0100

    Zmiana nazwy klasy zarzadzajacej workerami

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/workers/helpers/WorkerRunnerManager.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/workers/helpers/WorkerRunnerManager.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 import edu.p.lodz.pl.studentshenchman.factories.WorkerFactory;
 import edu.p.lodz.pl.studentshenchman.workers.AbstractWorker;
 import edu.p.lodz.pl.studentshenchman.workers.utils.WorkerType;
+import rx.Subscription;
 
 import static edu.p.lodz.pl.studentshenchman.workers.AbstractWorker.WORKER_NAME;
 
@@ -15,15 +16,15 @@ import static edu.p.lodz.pl.studentshenchman.workers.AbstractWorker.WORKER_NAME;
  * Created by Michał on 2016-10-24.
  */
 
-public class WorkerRunnerHelper {
-	private static final String TAG = WorkerRunnerHelper.class.getName();
+public class WorkerRunnerManager {
+	private static final String TAG = WorkerRunnerManager.class.getName();
 
 	public static void startWorker(Context context, Bundle bundle) {
 		WorkerType workerType = WorkerType.valueOf(bundle.getString(WORKER_NAME));
 		AbstractWorker abstractWorker = WorkerFactory.produce(context, workerType, bundle);
 		Log.i(TAG, "Uruchamianie nowego workera typu: " + workerType.name() + " klasy: " + abstractWorker.getClass().getName());
 		try {
-			abstractWorker.run();
+			Subscription subscription = abstractWorker.run();
 		} catch (Exception e) {
 			Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
 		}
