90d829101e2e2a365619403e8bd6d679307c8a61
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-15 23:25:40 +0100

    Poprawa bledu z widocznoscia floating buttona do dodawania notatek

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/fragments/SubjectDetailsFragment.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/timetable_plan/fragments/SubjectDetailsFragment.java
@@ -92,7 +92,10 @@ public class SubjectDetailsFragment extends StudentShenchmanMainFragment {
 
 		@Override
 		public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-			if (firstVisibleItem + visibleItemCount >= totalItemCount - visibleItemCount) {
+			if (totalItemCount <= visibleItemCount) {
+				AnimationHelper.startSlideBottomAnimation(mAddSubjectNoteFAB);
+				mAddSubjectNoteFAB.setVisibility(View.VISIBLE);
+			} else if (firstVisibleItem + visibleItemCount >= totalItemCount - visibleItemCount) {
 				mAddSubjectNoteFAB.setVisibility(View.GONE);
 			} else {
 				AnimationHelper.startSlideBottomAnimation(mAddSubjectNoteFAB);
