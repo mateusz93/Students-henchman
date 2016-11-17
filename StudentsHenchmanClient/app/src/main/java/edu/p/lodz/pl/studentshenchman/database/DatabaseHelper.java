03dde5254ec64bd5450b09c64ebea8a81c86793b
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-17 19:16:27 +0100

    Dodanie implementacji modelu ErrorReport oraz podbicie wersji bazy danych

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/database/DatabaseHelper.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/database/DatabaseHelper.java
@@ -29,7 +29,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	private static DatabaseHelper databaseHelper;
 
 	private static final String DATABASE_NAME = "StudentShenchman";
-	private static final int DATABASE_VERSION = 10;
+	private static final int DATABASE_VERSION = 11;
 
 	private static final String DROP_TABLE = "drop table if exists ";
 	private static final String CREATE_TABLE = "create table ";
@@ -48,6 +48,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 			put(DeanGroup.TABLE_NAME, createTableQuery(DeanGroup.TABLE_NAME, DeanGroup.COLUMN_NAMES, DeanGroup.COLUMN_TYPES));
 			put(Date.TABLE_NAME, createTableQuery(Date.TABLE_NAME, Date.COLUMN_NAMES, Date.COLUMN_TYPES));
 			put(Note.TABLE_NAME, createTableQuery(Note.TABLE_NAME, Note.COLUMN_NAMES, Note.COLUMN_TYPES));
+			put(Date.TABLE_NAME, createTableQuery(Date.TABLE_NAME, Date.COLUMN_NAMES, Date.COLUMN_TYPES));
 		}
 	};
 
--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/database/models/ErrorReport.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/database/models/ErrorReport.java
@@ -2,9 +2,41 @@ package edu.p.lodz.pl.studentshenchman.database.models;
 
 import android.provider.BaseColumns;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Michał on 2016-11-17.
  */
 
 public class ErrorReport implements BaseColumns {
+	private static final String TAG = ErrorReport.class.getName();
+
+	public static final String TABLE_NAME = "error_report";
+
+	public static final String _ID = "_id";
+	public static final String CONTENT = "content";
+	public static final String DATE = "date";
+
+	public static final Map<String, String> projectionColumns;
+
+	static {
+		projectionColumns = new HashMap<>();
+		projectionColumns.put(_ID,
+				_ID);
+		projectionColumns.put(CONTENT,
+				CONTENT);
+		projectionColumns.put(DATE,
+				DATE);
+
+	}
+
+	public static final String[] COLUMN_NAMES = {_ID, CONTENT, DATE};
+
+
+	public static final String[] COLUMN_TYPES = {
+			"integer primary key autoincrement",    //BUILD_ID
+			"text",                                 //CONTENT
+			"long"                                  //DATE
+	};
 }
