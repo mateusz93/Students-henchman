b642855c98c331677d767cf6131a2613f74ab50e
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-17 23:22:53 +0100

    Zmieniony skrypt 4. Zmieniony widok. Teraz zawiera id'ki TEACHERA oraz DEAN_GROUP Teraz tworzenie tabeli COURSE jest w skrypcie 1. Dane tabeli COURSE sa uzupelniane na podstawie widoku

--- StudentsHenchmanServiceBus/sql/4_create_from_mapping.sql
+++ StudentsHenchmanServiceBus/sql/4_create_from_mapping.sql
@@ -2,54 +2,38 @@ use henchman; #kazdy musi sobie usatwic swoja nazwe schematu
 SET foreign_key_checks = 0;
 
 drop view if exists v;
-drop table if exists COURSE;
 
-# ten widok budowany jest z tabel na razie jest tak aby byl poniewaz brakuje danych i na ten sprint raczej nie bedziemy go uzywac
-#przed zbudowaniem widoku nalezy wykonac skrypt z inserty_z_csv aby tabela plan_mapping miala wartosci
+#Przeniesiony insert z pliku nr 5 poniewaz teraz wplywa on na wartosci w widoku wiec musi byc wykonany przed tworzeniem widoku
+INSERT INTO TEACHER (NAME)
+  SELECT DISTINCT PLAN_MAPPING.Surname
+  FROM PLAN_MAPPING WHERE Surname is not null and Surname not in('Surname','');
+  
+
+
+#tworzenie widoku z tabeli PLAN_MAPPING joinowanej z TEACHER oraz DEAN_GROUP (jak na razie)
+
 create view v AS 
 (
-select `p`.`id` AS `id`,
-`p`.`Ref` AS `Ref`,
-`p`.`Day` AS `Day`,
-`p`.`Time` AS `Time`,
-`p`.`Weeks` AS `Weeks`,
-`p`.`EventCat` AS `EventCat`,
-`p`.`Surname` AS `Surname`,
-`p`.`Module` AS `Module`,
-`g`.`ABBREVIATION` AS `abbreviation`,
-`g`.`NAME` AS `group_name`
-from (`PLAN_MAPPING` `p` join `DEAN_GROUP` `g`
-on(((`g`.`ABBREVIATION` = `p`.`Group`) or (`g`.`ABBREVIATION` = `p`.`Student`)))));
-
-
-# ta tabela budowana z widoku na razie jest tak aby byla poniewaz brakuje danych i na ten sprint raczej jej nie bedziemy uzywac
-CREATE table COURSE as
 select 
-id as ID,
-Ref as EXTERNAL_ID,
-Day as DAY,
-Time as TIME,
-Weeks as WEEKS,
-Module as NAME,
-Surname as TEACHER_NAME,
-abbreviation as ABBREVIATION,
-group_name as GROUP_NAME
-from v;
+p.id AS id,
+p.Ref AS ref,
+p.Day AS day,
+p.Time AS time,
+p.Weeks AS weeks,
+p.EventCat AS event_type,
+t.ID AS teacher_id,
+p.Module AS module_name,
+g.ID AS dean_group_id
+from (PLAN_MAPPING p
+join DEAN_GROUP g
+on ((g.ABBREVIATION = p.Group) or (g.ABBREVIATION = p.Student)))
+join TEACHER t
+on p.Surname = t.NAME);
 
-
-#**************************************************
-#create tabeli course jakby ktos bardzo potrzebowal ale domyslnie jest wykomentowany ;)
-#CREATE TABLE `COURSE` (
-#  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-#  `EXTERNAL_ID` varchar(100),
-#  `DAY` varchar(100),
-#  `TIME` varchar(100),
-#  `WEEKS` varchar(100),
-#  `NAME` varchar(100),
-#  `TEACHER_NAME` varchar(100),
-#  `ABBREVIATION` varchar(255),
-#  `GROUP_NAME` varchar(255)
-#) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-#**************************************************
+#Po utworzeniu widoku v nalezy wykonac tego inserta w celu uzupelnienia tabeli COURSE
+INSERT INTO COURSE (EXTERNAL_ID, NAME, TIME, DAY, WEEKS, DEAN_GROUP_ID, TEACHER_ID)
+  SELECT v.ref, v.module_name, v.time, v.day, v.weeks, v.dean_group_id, v.teacher_id
+   FROM v;
+  
 
 SET foreign_key_checks = 1;
