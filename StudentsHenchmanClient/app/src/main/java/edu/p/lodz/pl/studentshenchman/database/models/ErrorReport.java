3a4d81834dca4d31e27e0ed61c436f5b9e3e1dc0
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-17 19:10:11 +0100

    Inicjalizacja modelu przetrzymujacego error reporty, np. z crash'y aplikacji

---
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/database/models/ErrorReport.java
@@ -0,0 +1,10 @@
+package edu.p.lodz.pl.studentshenchman.database.models;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Michał on 2016-11-17.
+ */
+
+public class ErrorReport implements BaseColumns {
+}
