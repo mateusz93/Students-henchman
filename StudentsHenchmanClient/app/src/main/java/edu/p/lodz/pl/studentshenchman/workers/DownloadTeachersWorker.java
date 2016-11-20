6d733a83f1a397185ba89fb523f9205bc83ce323
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-20 13:00:05 +0100

    Implementacja workera do pobierania wykladowcow

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/workers/DownloadTeachersWorker.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/workers/DownloadTeachersWorker.java
@@ -1,10 +1,21 @@
 package edu.p.lodz.pl.studentshenchman.workers;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.List;
 
 import cdm.TeacherRS;
+import edu.p.lodz.pl.studentshenchman.database.DatabaseHelper;
+import edu.p.lodz.pl.studentshenchman.database.models.Teacher;
+import edu.p.lodz.pl.studentshenchman.workers.endpoints.TeachersEndpoints;
+import edu.p.lodz.pl.studentshenchman.workers.factories.ServiceFactory;
+import rx.Observable;
 import rx.Subscription;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by Michał on 2016-11-20.
@@ -19,21 +30,44 @@ public class DownloadTeachersWorker extends AbstractWorker<TeacherRS> {
 
 	@Override
 	public Subscription run() {
-		return null;
+		Toast.makeText(mContext, "Starting download teachers", Toast.LENGTH_SHORT).show();
+		TeachersEndpoints settingsEndpoints = ServiceFactory.produceService(TeachersEndpoints.class, false);
+		Observable<TeacherRS> call = settingsEndpoints.getTeachers();
+
+		Subscription subscription = call.subscribeOn(Schedulers.newThread())
+				.observeOn(Schedulers.newThread())
+				.subscribe(this);
+
+		return subscription;
 	}
 
 	@Override
 	public void onCompleted() {
-
+		Log.i(TAG, "Teachers downloaded successfully");
+		Toast.makeText(mContext, "Teachers downloaded successfully", Toast.LENGTH_SHORT).show();
+		notifyTaskFinished(FinishedWorkerStatus.SUCCESS);
 	}
 
 	@Override
 	public void onError(Throwable e) {
-
+		onError(mContext, e);
+		notifyTaskFinished(FinishedWorkerStatus.FAIL);
 	}
 
 	@Override
 	public void onNext(TeacherRS teacherRS) {
+		Log.i(TAG, "Saving teachers downloaded from server");
+		SQLiteDatabase db = DatabaseHelper.getInstance(mContext).getWritableDatabase();
+		deleteOldTeachers(db);
+		saveNewTeachers(db, teacherRS.getTeachers());
+	}
 
+	private void saveNewTeachers(SQLiteDatabase db, List<model.Teacher> teachers) {
+		for (model.Teacher teacherDto : teachers)
+			db.insert(Teacher.TABLE_NAME, null, Teacher.fromDTO2CV(teacherDto));
+	}
+
+	private void deleteOldTeachers(SQLiteDatabase db) {
+		db.delete(Teacher.TABLE_NAME, null, null);
 	}
 }
