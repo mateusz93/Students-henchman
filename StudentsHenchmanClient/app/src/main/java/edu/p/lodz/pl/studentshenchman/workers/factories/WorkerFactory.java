6fb3e4c59b0f1b79ff537b74feecb3b3043b61e0
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-21 22:30:59 +0100

    Dodanie nowych typow workerow

--- StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/workers/factories/WorkerFactory.java
+++ StudentsHenchmanClient/app/src/main/java/edu/p/lodz/pl/studentshenchman/workers/factories/WorkerFactory.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.os.Bundle;
 
 import edu.p.lodz.pl.studentshenchman.workers.AbstractWorker;
+import edu.p.lodz.pl.studentshenchman.workers.AddNoteWorker;
+import edu.p.lodz.pl.studentshenchman.workers.DeleteNoteWorker;
 import edu.p.lodz.pl.studentshenchman.workers.DownloadDateWorker;
 import edu.p.lodz.pl.studentshenchman.workers.DownloadSettingsWorker;
 import edu.p.lodz.pl.studentshenchman.workers.DownloadTeachersWorker;
 import edu.p.lodz.pl.studentshenchman.workers.DownloadTimeTableWorker;
 import edu.p.lodz.pl.studentshenchman.workers.IdleWorker;
+import edu.p.lodz.pl.studentshenchman.workers.SetUserPreferencesWorker;
 import edu.p.lodz.pl.studentshenchman.workers.utils.WorkerType;
 
 /**
@@ -22,10 +25,16 @@ public class WorkerFactory {
 				return new DownloadDateWorker(context, bundle);
 			case DOWNLOAD_SETTINGS:
 				return new DownloadSettingsWorker(context, bundle);
-			case DOWNLOAD_TIMETABLE:
+			case DOWNLOAD_COURSES:
 				return new DownloadTimeTableWorker(context, bundle);
 			case DOWNLOAD_TEACHERS:
 				return new DownloadTeachersWorker(context, bundle);
+			case SET_USER_PREFERENCES:
+				return new SetUserPreferencesWorker(context, bundle);
+			case ADD_NOTE:
+				return new AddNoteWorker(context, bundle);
+			case DELETE_NOTE:
+				return new DeleteNoteWorker(context, bundle);
 			default:
 				return new IdleWorker(context, bundle);
 		}
